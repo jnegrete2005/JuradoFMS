f4cc1bd3df8bc0257059fb37afe7f1bfc41b6b5f
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-01-23 15:22:32 -0500

    Set up admin page

--- vote/admin.py
+++ vote/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Competitor, VotingPoll
+
 # Register your models here.
+class CompetitorAdmin(admin.ModelAdmin):
+  list_display = (
+    'id', 'name', 'easy', 'hard', 'tematicas', 'random_mode', 'random_score', 'min1', 'min2', 'deluxe', 'replica'
+  )
+
+class VotingPollAdmin(admin.ModelAdmin):
+  list_display = ( 'comp_1', 'comp_2' )
+
+
+admin.site.register(Competitor, CompetitorAdmin)
+admin.site.register(VotingPoll, VotingPollAdmin)
