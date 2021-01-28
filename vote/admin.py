6d6136dc933807f7c95d0a671394a5ca8705cbf8
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-01-28 10:40:52 -0500

    Use decorators to register models in admin site

--- vote/admin.py
+++ vote/admin.py
@@ -3,14 +3,13 @@ from django.contrib import admin
 from .models import Competitor, VotingPoll
 
 # Register your models here.
+@admin.register(Competitor)
 class CompetitorAdmin(admin.ModelAdmin):
   list_display = (
     'id', 'name', 'easy', 'hard', 'tematicas', 'random_mode', 'random_score', 'min1', 'min2', 'deluxe', 'replica'
   )
 
+
+@admin.register(VotingPoll)
 class VotingPollAdmin(admin.ModelAdmin):
   list_display = ( 'comp_1', 'comp_2' )
-
-
-admin.site.register(Competitor, CompetitorAdmin)
-admin.site.register(VotingPoll, VotingPollAdmin)
