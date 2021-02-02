a50ee7b84b64063602443777c20a94db3677ac87
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-02-02 17:00:41 -0500

    A file to validate all forms

---
+++ vote/static/vote/TypeScript/forms.ts
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form: HTMLFormElement) {
+    form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})
