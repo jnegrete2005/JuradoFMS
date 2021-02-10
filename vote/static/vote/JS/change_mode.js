d27341b75ddf543ee08cb4ec93da7c3014d42820
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-02-10 13:34:13 -0500

    File for when the user changes modes

---
+++ vote/static/vote/JS/change_mode.js
@@ -0,0 +1,73 @@
+import { addInputs, getCookie } from "./util";
+export function changeMode(mode, replica) {
+    // Change the current mode
+    document.getElementById('mode').dataset.current_mode = mode;
+    document.getElementById('mode').innerHTML = modes_aliases[mode];
+    // Changing the Navbar
+    const nav_links = document.getElementsByClassName('nav-link');
+    Array.prototype.forEach.call(nav_links, (link) => {
+        if (mode === link.dataset.mode) {
+            link.classList.add('active');
+        }
+        if (replica === true) {
+            link.classList.toggle('disabled', link.dataset.mode !== 'replica');
+        }
+        else {
+            link.classList.remove('disabled');
+        }
+    });
+    // Fetching to fill the inputs
+    const query = `
+    query GetModes($id1: ID!, $id2: ID!, $mode: String!) {
+      comp1: compMode(id: $id1, mode: $mode) {
+        mode
+      }
+      comp2: compMode(id: $id2, mode: $mode) {
+        mode
+      }
+    }
+  `;
+    fetch('/graphql/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-CSRFToken': getCookie('csrftoken')
+        },
+        body: JSON.stringify({
+            query,
+            variables: {
+                id1: JSON.parse(localStorage.getItem('comp_1')).id,
+                id2: JSON.parse(localStorage.getItem('comp_2')).id,
+                mode: mode
+            }
+        }),
+        credentials: 'include'
+    })
+        .then(response => {
+        if (!response.ok) {
+            throw Error(`${response.statusText} - ${response.url}`);
+        }
+        return response.json();
+    })
+        .then((data) => {
+        if (data.data.comp1 !== null) {
+            addInputs(data.data.comp1.length, data);
+        }
+    });
+}
+window.addEventListener('popstate', event => {
+});
+const modes_aliases = {
+    easy: 'Easy Mode',
+    hard: 'Hard Mode',
+    tematicas: 'Temáticas',
+    random_mode: 'Random Mode',
+    min1: 'Primer Minuto',
+    min2: 'Segundo Munuto',
+    deluxe: 'Deluxe',
+    replica: 'Réplica'
+};
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
