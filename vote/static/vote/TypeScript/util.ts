fd258be23ab2b4273d1aeb6e0d4df35c0778f142
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-02-10 13:30:02 -0500

    Modify addInputs

--- vote/static/vote/TypeScript/util.ts
+++ vote/static/vote/TypeScript/util.ts
@@ -1,3 +1,5 @@
+import { GetModes } from "./change_mode";
+
 export function getCookie(name: string): string {
   let cookieValue: null | string = null;
 
@@ -29,20 +31,57 @@ export function useModal(title: string, body: string):void {
   })
 }
 
-export function addInputs(lenght: number) {
+export function addInputs(lenght: number, data?: GetModes) {
+  if (data) {
+    data.data.comp1.reverse()
+    data.data.comp2.reverse()
+  }
+
   Array.prototype.forEach.call(document.getElementsByClassName('comp-container'), (comp_container: HTMLElement) => {
     for (let i = 0; i < lenght; i++) {
+      // Create the container and add the classes
       const container = document.createElement('div')
       container.classList.add(
-        'col', 'col-md', 'd-flex', 'justify-content-center', 'justify-content-lg-left'
+        'col-4', 'col-md', 'd-flex', 'justify-content-center', 'justify-content-lg-left', 'input'
       )
+      
+      if (i > lenght - 3 && lenght % 3 !== 0) {
+        if (Math.round(lenght % 3) === 2) {
+          container.classList.replace('col-4', 'col-6')
+        } else if (Math.round(lenght % 3) === 1 && i > lenght - 2) {
+          console.log(container.classList.replace('col-4', 'col')) 
+        }
+      }
 
+      // Create the inputs
       const input = document.createElement('input')
       input.type = 'number'
-      input.classList.add('form-control')
+      if (comp_container.id === 'comp-1-container') {
+        input.classList.add('form-control', 'comp-1-input')
+      } else if (comp_container.id === 'comp-2-container') {
+        input.classList.add('form-control', 'comp-2-input')
+      }
       container.append(input)
 
-      insertAfter(container, comp_container)
+      // Populate inputs
+      if (data) {
+        if (comp_container.id === 'comp-1-container') {
+          if (data.data.comp1 !== null && data.data.comp1[i] !== 9) {
+            input.value = data.data.comp1[i].toString()
+          }
+        } else if (comp_container.id === 'comp-2-container') {
+          if (data.data.comp2 !== null && data.data.comp2[i] !== 9) {
+            input.value = data.data.comp2[i].toString()
+          }
+        }
+      }
+
+      // Insert the container after the competitor
+      if (i === 0) {
+        insertAfter(container, comp_container)
+      } else {
+        insertAfter(container, document.getElementsByClassName('input')[document.getElementsByClassName('input').length - 1] as HTMLElement)  
+      }
     }
   })
 }
