9d89463964d9b1e5fc1e96deed33f3930c923f65
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-02-10 13:33:34 -0500

    Remove unreachable code

--- vote/static/vote/JS/navbar.js
+++ vote/static/vote/JS/navbar.js
@@ -1,16 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const mode = document.getElementById('mode').dataset.mode;
+    const mode = document.getElementById('mode').dataset.current_mode;
     const nav_links = document.getElementsByClassName('nav-link');
     if (mode === '') {
         Array.prototype.forEach.call(nav_links, (link) => {
             link.classList.add('disabled');
-        });
-    }
-    else {
-        Array.prototype.forEach.call(nav_links, (link) => {
-            if (link.dataset.mode === mode) {
-                link.classList.add('active');
-            }
         });
     }
 });
--- vote/static/vote/TypeScript/navbar.ts
+++ vote/static/vote/TypeScript/navbar.ts
@@ -1,16 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const mode = document.getElementById('mode').dataset.mode
+  const mode = document.getElementById('mode').dataset.current_mode
 
   const nav_links = document.getElementsByClassName('nav-link');
   if (mode === '') {
     Array.prototype.forEach.call(nav_links, (link: HTMLAnchorElement) => {
       link.classList.add('disabled')
-    })
-  } else {
-    Array.prototype.forEach.call(nav_links, (link: HTMLAnchorElement) => {
-      if (link.dataset.mode === mode) {
-        link.classList.add('active');
-      }
     })
   }
 })
