fc0f7480d3ebe77371b887f92a47dcb1a0b21c95
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-02-14 22:30:10 -0500

    Add comp query

--- vote/graphql/query.py
+++ vote/graphql/query.py
@@ -1,14 +1,15 @@
-from .model_types import CompetitorType, CompetitorModeType, VotingPollType
+from .types import CompetitorType, VotingPollType
 from ..models import Competitor, VotingPoll
 
 import graphene
 
 class Query(graphene.ObjectType):
   voting_poll = graphene.Field(VotingPollType, id=graphene.ID())
-  comp_mode = graphene.Field(CompetitorModeType, id=graphene.ID(), mode=graphene.String())
+  comp = graphene.Field(CompetitorType, id=graphene.ID())
 
   def resolve_voting_poll(root, info, id):
     return VotingPoll.objects.get(pk=id)
 
-  def resolve_comp_mode(root, info, id, mode):
-    return Competitor.objects.get(pk=id).__dict__[mode]
+  def resolve_comp(root, info, id):
+    return Competitor.objects.get(pk=id)
+    
