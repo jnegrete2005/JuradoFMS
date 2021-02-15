a0765f8c6e23289b8e80dbf676a011e865d9b6d8
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-02-14 22:29:40 -0500

    Rename CreateComps to CreatePoll. Add SaveMode

--- vote/graphql/mutations.py
+++ vote/graphql/mutations.py
@@ -1,23 +1,42 @@
-from .model_types import CompetitorType, VotingPollType
+from .types import CompetitorType, VotingPollType
 from ..models import Competitor, VotingPoll
 
 import graphene
 
-class CreateCompetitors(graphene.Mutation):
+class CreatePoll(graphene.Mutation):
   class Arguments:
-    comp_1 = graphene.String(required=True)
-    comp_2 = graphene.String(required=True)
+    comp1 = graphene.String(required=True)
+    comp2 = graphene.String(required=True)
 
-  comp_1 = graphene.Field(CompetitorType)
-  comp_2 = graphene.Field(CompetitorType)
+  poll = graphene.Field(VotingPollType)
 
   @classmethod
-  def mutate(cls, root, info, comp_1, comp_2):
-    comp_1 = Competitor.objects.create(name=comp_1)
-    comp_2 = Competitor.objects.create(name=comp_2)
+  def mutate(cls, root, info, comp1, comp2):
+    comp_1 = Competitor.objects.create(name=comp1)
+    comp_2 = Competitor.objects.create(name=comp2)
+
+    poll = VotingPoll.objects.create(comp_1=comp_1, comp_2=comp_2)
     
-    return CreateCompetitors(comp_1=comp_1, comp_2=comp_2)
+    return CreatePoll(poll=poll)
+
+
+class SaveMode(graphene.Mutation):
+  class Arguments:
+    id = graphene.ID(required=True)
+    mode = graphene.String(required=True)
+    value = graphene.List(graphene.Int, required=True)
+
+  comp = graphene.Field(CompetitorType)
+
+  @classmethod
+  def mutate(cls, root, info, id, mode, value):
+    comp = Competitor.objects.get(pk=id)
+    comp.__dict__[mode] = value
+    comp.save(update_fields=[mode])
+
+    return SaveMode(comp=comp)
 
 
 class Mutation(graphene.ObjectType):
-  create_competitors = CreateCompetitors.Field()
+  create_poll = CreatePoll.Field()
+  save_mode = SaveMode.Field()
