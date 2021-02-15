f04e98e2042b94920f4abd980976378a51d89803
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-02-14 22:37:29 -0500

    Change query from changeMode. Add steps. Add saveMode

--- vote/static/vote/JS/change_mode.js
+++ vote/static/vote/JS/change_mode.js
@@ -1,32 +1,59 @@
-import { addInputs, getCookie } from "./util";
-export function changeMode(mode, replica) {
-    // Change the current mode
-    document.getElementById('mode').dataset.current_mode = mode;
-    document.getElementById('mode').innerHTML = modes_aliases[mode];
-    // Changing the Navbar
-    const nav_links = document.getElementsByClassName('nav-link');
-    Array.prototype.forEach.call(nav_links, (link) => {
-        if (mode === link.dataset.mode) {
-            link.classList.add('active');
+import { prepareNavbar } from "./navbar";
+import { VotingPoll } from "./types";
+import { addInputs, createAlert, getCookie } from "./util";
+function saveMode(mode) {
+    // Create the mutation
+    const mutation = `
+    mutation SaveMode($id1: ID!, $id2: ID!, $mode: String!, $value1: [Int]!, $value2: [Int]!) {
+      comp1: saveMode(id: $id1, mode: $mode, value: $value1) {
+        comp {
+          ${mode}
         }
-        if (replica === true) {
-            link.classList.toggle('disabled', link.dataset.mode !== 'replica');
-        }
-        else {
-            link.classList.remove('disabled');
-        }
-    });
-    // Fetching to fill the inputs
-    const query = `
-    query GetModes($id1: ID!, $id2: ID!, $mode: String!) {
-      comp1: compMode(id: $id1, mode: $mode) {
-        mode
       }
-      comp2: compMode(id: $id2, mode: $mode) {
-        mode
+      comp2: saveMode(id: $id2, mode: $mode, value: $value2) {
+        comp {
+          ${mode}
+        }
       }
     }
   `;
+    // Fetch
+    fetch('/graphql/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-CSRFToken': getCookie('csrftoken')
+        },
+        body: JSON.stringify({
+            query: mutation,
+            variables: {
+                "id1": VotingPoll.unserialize(localStorage.getItem('poll')).comp_1.id,
+                "id2": VotingPoll.unserialize(localStorage.getItem('poll')).comp_2.id,
+                "mode": mode,
+            }
+        })
+    });
+}
+function changeMode(mode) {
+    // Change the current mode
+    document.getElementById('mode').dataset.current_mode = mode;
+    document.getElementById('mode').innerHTML = modes_aliases[mode];
+    // Create the query
+    const query = `
+    query GetModes($pollID: ID!) {
+      votingPoll(id: $pollID) {
+        id
+        comp1 {
+          mode: ${mode}
+        }
+        comp2 {
+          mode: ${mode}
+        }
+      }
+    }
+  `;
+    // Fetch
     fetch('/graphql/', {
         method: 'POST',
         headers: {
@@ -51,13 +78,42 @@ export function changeMode(mode, replica) {
         return response.json();
     })
         .then((data) => {
+        // Fill the inputs
         if (data.data.comp1 !== null) {
             addInputs(data.data.comp1.length, data);
         }
+        else {
+            switch (mode) {
+                case 'tematicas':
+                    addInputs(7);
+                    break;
+                case 'deluxe':
+                    addInputs(14);
+                    break;
+                default:
+                    addInputs(9);
+                    break;
+            }
+        }
+        // Create the alert 
+        createAlert('Recuerda que los últimos 3 cuadritos siempre son para Skills, Flow y Puesta en escena');
+        // Fill the heading with the mode
+        document.getElementById('mode').dataset.current_mode = mode;
+        document.getElementById('mode').innerHTML = modes_aliases[mode];
+        // Prepare the navbar
+        prepareNavbar(mode);
     });
 }
-window.addEventListener('popstate', event => {
-});
+const modes_to_int = {
+    easy: 0,
+    hard: 1,
+    tematicas: 2,
+    random_mode: 3,
+    min1: 4,
+    min2: 5,
+    deluxe: 6,
+    replica: 7
+};
 const modes_aliases = {
     easy: 'Easy Mode',
     hard: 'Hard Mode',
--- vote/static/vote/TypeScript/change_mode.ts
+++ vote/static/vote/TypeScript/change_mode.ts
@@ -1,36 +1,67 @@
-import { addInputs, getCookie } from "./util";
+import { prepareNavbar } from "./navbar";
+import { VotingPoll } from "./types";
+import { addInputs, createAlert, getCookie } from "./util";
 
-export function changeMode(mode: string, replica?: boolean): void {
-  // Change the current mode
-  document.getElementById('mode').dataset.current_mode = mode
-  document.getElementById('mode').innerHTML = modes_aliases[mode]
-
-  // Changing the Navbar
-  const nav_links = document.getElementsByClassName('nav-link');
-  Array.prototype.forEach.call(nav_links, (link: HTMLAnchorElement) => {
-    if (mode === link.dataset.mode) {
-      link.classList.add('active')
-    }
-
-    if (replica === true) {
-      link.classList.toggle('disabled', link.dataset.mode !== 'replica')
-    } else {
-      link.classList.remove('disabled')
-    }
-  })
-
-  // Fetching to fill the inputs
-  const query = `
-    query GetModes($id1: ID!, $id2: ID!, $mode: String!) {
-      comp1: compMode(id: $id1, mode: $mode) {
-        mode
+function saveMode(mode: string): void {
+  // Create the mutation
+  const mutation = `
+    mutation SaveMode($id1: ID!, $id2: ID!, $mode: String!, $value1: [Int]!, $value2: [Int]!) {
+      comp1: saveMode(id: $id1, mode: $mode, value: $value1) {
+        comp {
+          ${mode}
+        }
       }
-      comp2: compMode(id: $id2, mode: $mode) {
-        mode
+      comp2: saveMode(id: $id2, mode: $mode, value: $value2) {
+        comp {
+          ${mode}
+        }
       }
     }
   `
 
+  // Fetch
+  fetch('/graphql/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'X-CSRFToken': getCookie('csrftoken')
+    },
+    body: JSON.stringify({
+      query: mutation,
+      variables: {
+        "id1": VotingPoll.unserialize(localStorage.getItem('poll')).comp_1.id,
+        "id2": VotingPoll.unserialize(localStorage.getItem('poll')).comp_2.id,
+        "mode": mode,
+        // "value1": [1,2,1,1,1,1,1,1,1],
+        // "value2": [1,2,1,1,1,1,1,1,1]
+        // TODO
+      }
+    })
+  })
+}
+
+function changeMode(mode: string): void {
+  // Change the current mode
+  document.getElementById('mode').dataset.current_mode = mode
+  document.getElementById('mode').innerHTML = modes_aliases[mode]
+
+  // Create the query
+  const query = `
+    query GetModes($pollID: ID!) {
+      votingPoll(id: $pollID) {
+        id
+        comp1 {
+          mode: ${mode}
+        }
+        comp2 {
+          mode: ${mode}
+        }
+      }
+    }
+  `
+
+  // Fetch
   fetch('/graphql/', {
     method: 'POST',
     headers: {
@@ -55,15 +86,47 @@ export function changeMode(mode: string, replica?: boolean): void {
     return response.json()
   })
   .then((data: GetModes) => {
+    // Fill the inputs
     if (data.data.comp1 !== null) {
       addInputs(data.data.comp1.length, data)
+    } else {
+      switch (mode) {
+        case 'tematicas':
+          addInputs(7)
+          break
+
+        case 'deluxe':
+          addInputs(14)
+          break
+
+        default:
+          addInputs(9)
+          break
+      }
     }
+
+    // Create the alert 
+    createAlert('Recuerda que los últimos 3 cuadritos siempre son para Skills, Flow y Puesta en escena')
+    
+    // Fill the heading with the mode
+    document.getElementById('mode').dataset.current_mode = mode
+    document.getElementById('mode').innerHTML = modes_aliases[mode]
+    
+    // Prepare the navbar
+    prepareNavbar(mode)
   })
 }
 
-window.addEventListener('popstate', event => {
-
-})
+const modes_to_int = {
+  easy: 0,
+  hard: 1,
+  tematicas: 2,
+  random_mode: 3,
+  min1: 4,
+  min2: 5,
+  deluxe: 6,
+  replica: 7
+}
 
 const modes_aliases = {
   easy: 'Easy Mode',
@@ -76,7 +139,7 @@ const modes_aliases = {
   replica: 'Réplica'
 }
 
-function getKeyByValue(object: object, value: string) { 
+function getKeyByValue(object: object, value: string | number) { 
   return Object.keys(object).find(key => object[key] === value); 
 }
 
