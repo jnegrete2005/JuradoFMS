8e458302c4e44d2cad65e964b837da1890e1d976
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-02-14 22:31:48 -0500

    Create tests for queries

---
+++ vote/tests/test_queries.py
@@ -0,0 +1,92 @@
+import json
+
+from graphene_django.utils.testing import GraphQLTestCase
+
+from ..models import Competitor, VotingPoll
+
+# Create your tests here.
+class QueriesTestCase(GraphQLTestCase):
+  @classmethod
+  def setUpTestData(cls):
+    # Create Competitors
+    c1_a = Competitor.objects.create(
+      name='Si',
+      easy=[1,1,1,1,1,1,1,1,1],
+      hard=[1,1,1,1,1,1,1,1,1],
+      tematicas=[1,1,1,1,1,1,1],
+      random_mode=1,
+      random_score=[1,1,1,1,1,1,1,1,1],
+      min1=[1,1,1,1,1,1,1,1,1],
+      min2=[1,1,1,1,1,1,1,1,1],
+      deluxe=[1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    )
+    c1_b =  Competitor.objects.create(
+      name='No',
+      easy=[2,2,9,2,2,2,2,2,9],
+      hard=[2,2,2,2,2,2,2,2,2],
+      tematicas=[2,2,2,2,2,2,2],
+      random_mode=2,
+      random_score=[2,2,2,2,2,2,2,2,2],
+      min1=[2,2,2,2,2,2,2,2,2],
+      min2=[2,2,2,2,2,2,2,2,2],
+      deluxe=[2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+    )
+    c2_a = Competitor.objects.create(
+      name='Replica 1',
+      easy=[3,3,3,3,3,3,3,3,3],
+      hard=[3,3,3,3,3,3,3,3,3],
+      tematicas=[3,3,3,3,3,3,3],
+      random_mode=3,
+      random_score=[3,3,3,3,3,3,3,3,3],
+      min1=[3,3,3,3,3,3,3,3,3],
+      min2=[3,3,3,3,3,3,3,3,3],
+      deluxe=[3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+      replica=[3,3,3,3,3,3,3,9,9]
+    )
+    c2_b = Competitor.objects.create(
+      name='Replica 2',
+      easy=[3,3,3,3,3,3,3,3,9],
+      hard=[3,3,3,3,3,3,3,3,3],
+      tematicas=[3,3,3,3,3,3,3],
+      random_mode=3,
+      random_score=[3,3,3,3,3,3,3,3,3],
+      min1=[3,3,3,3,3,3,3,3,3],
+      min2=[3,3,3,3,3,3,3,3,3],
+      deluxe=[3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+      replica=[3,3,3,3,3,3,3,3,3]
+    )
+
+    # Create VotingPolls
+    poll1 = VotingPoll.objects.create(
+      comp_1=c1_a,
+      comp_2=c1_b
+    )
+    poll2 = VotingPoll.objects.create(
+      comp_1=c2_a,
+      comp_2=c2_b
+    )
+
+  def test_voting_poll(self):
+    """ Should return a complete VotingPoll """
+
+    response = self.query(
+      """ 
+      query votingPoll($id: ID!) {
+        votingPoll(id: $id) {
+          comp1 {
+            id
+            name
+          }
+          comp2 {
+            id
+            name
+          }
+        }
+      }
+      """,
+      variables={ 'id': VotingPoll.objects.first().id }
+    )
+    content = json.loads(response.content)    
+    self.assertResponseNoErrors(response)
+    self.assertEqual(content['data']['votingPoll']['comp1']['name'], 'Si')
+  
