69dccd5de7f3308ba17bf6a2c0263c9641dd9b8c
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-02-26 09:52:54 -0500

    Add beggining for changing a mode

--- vote/static/vote/TypeScript/popstate.ts
+++ vote/static/vote/TypeScript/popstate.ts
@@ -1,10 +1,17 @@
 import { prepareNavbar } from "./navbar.js"
+import { saveMode } from "./change_mode.js"
 
 window.addEventListener('popstate', event => {
   if (event.state !== null) {
     if (event.state.mode !== null) {
       if (event.state.start !== null) {
         prepareNavbar('easy')
+      } else {
+        // Save the old mode
+        saveMode(event.state.old_mode)
+
+        prepareNavbar(event.state.new_mode)
+        
       }
     }
   }
