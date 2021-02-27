e93c48987c895569af8cdd2e221ade99ad71af46
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-02-26 19:27:55 -0500

    Fix popstate logical bug

--- vote/static/vote/TypeScript/popstate.ts
+++ vote/static/vote/TypeScript/popstate.ts
@@ -1,18 +1,13 @@
 import { prepareNavbar } from "./navbar.js"
-import { saveMode } from "./change_mode.js"
+import { changeMode } from "./change_mode.js"
 
-window.addEventListener('popstate', event => {
-  if (event.state !== null) {
-    if (event.state.mode !== null) {
-      if (event.state.start !== null) {
-        prepareNavbar('easy')
-      } else {
-        // Save the old mode
-        saveMode(event.state.old_mode)
-
-        prepareNavbar(event.state.new_mode)
-        
-      }
+window.onpopstate = (event: PopStateEvent) => {
+  if (event.state) {
+    if (event.state.start) {
+      prepareNavbar('easy')
+    }
+    if (event.state.new_mode) {
+      changeMode(event.state.old_mode, event.state.new_mode)
     }
   }
-})
+}
