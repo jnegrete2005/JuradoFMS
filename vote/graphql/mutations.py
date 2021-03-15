d7a53bae5a9f25e81315f70bcfc055aef48ebaff
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-03-15 00:29:24 -0500

    Add error handling

--- vote/graphql/mutations.py
+++ vote/graphql/mutations.py
@@ -1,7 +1,10 @@
 from .types import CompetitorType, VotingPollType, CompMode
 from ..models import Competitor, VotingPoll
 
+from graphql import GraphQLError
+
 import graphene
+
 
 class CreatePoll(graphene.Mutation):
   class Arguments:
@@ -28,7 +31,19 @@ class SaveModes(graphene.Mutation):
   comp_2 = graphene.Field(CompMode)
 
   @classmethod
-  def mutate(cls, root, info, poll_id, mode, value_1, value_2):
+  def mutate(cls, root, info, poll_id: int, mode: str, value_1: list, value_2: list):
+    # Check if the mode is according to the length
+    if mode == 'deluxe':
+      if len(value_1) != 14 or len(value_2) != 14:
+        raise GraphQLError('Deluxe no puede tener más ni menos de 14 elementos')
+
+    elif mode == 'tematicas':
+      if len(value_1) != 7 or len(value_2) != 7:
+        raise GraphQLError('Tematicas no puede tener más ni menos de 7 elementos')
+
+    elif len(value_1) != 9 or len(value_2) != 9:
+      raise GraphQLError(f'{Competitor._meta.get_field(mode).verbose_name} no puede tener más ni menos de 9 elementos')
+
     poll = VotingPoll.objects.get(pk=poll_id)
 
     poll.comp_1.__dict__[mode] = value_1
