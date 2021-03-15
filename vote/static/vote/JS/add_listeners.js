b4c899c771d2726aa904e0d82e187baf9a9d4110
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-03-15 02:14:25 -0500

    Add replica block, add imports

--- vote/static/vote/JS/add_listeners.js
+++ vote/static/vote/JS/add_listeners.js
@@ -1,6 +1,7 @@
 import { getKeyByValue } from "./util.js";
 import { modes_to_int } from "./types.js";
 import { changeMode } from "./change_mode.js";
+import { fillTable } from "./end.js";
 document.addEventListener('DOMContentLoaded', () => addListeners());
 function addListeners() {
     Array.from(document.getElementsByClassName('listen')).forEach((el) => {
@@ -10,6 +11,11 @@ function addListeners() {
             // Get the modes
             const old_mode = mode_el.dataset.current_mode;
             const new_mode = el.dataset.mode !== undefined ? el.dataset.mode : getKeyByValue(modes_to_int, modes_to_int[old_mode] + parseInt(el.dataset.op));
+            if (new_mode === 'replica') {
+                history.pushState({ end: true }, '', '#end');
+                fillTable();
+                return;
+            }
             // Push State
             history.pushState({ new_mode, old_mode }, '', `#${new_mode}`);
             changeMode(old_mode, new_mode);
