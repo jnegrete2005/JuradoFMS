4ebbbf6bda7d1dc9812d3b50ddd53482735a153f
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-03-19 00:23:23 -0500

    Delete old_mode from pushstate

--- vote/static/vote/JS/add_listeners.js
+++ vote/static/vote/JS/add_listeners.js
@@ -7,20 +7,19 @@ function addListeners() {
     Array.from(document.getElementsByClassName('listen')).forEach((el) => {
         el.addEventListener('click', async (event) => {
             event.preventDefault();
-            const mode_el = document.getElementById('mode');
             // Get the modes
-            const old_mode = mode_el.dataset.current_mode;
+            const old_mode = document.getElementById('mode').dataset.current_mode;
             const new_mode = el.dataset.mode !== undefined
                 ? el.dataset.mode
                 : getKeyByValue(modes_to_int, modes_to_int[old_mode] + parseInt(el.dataset.op));
             if (new_mode === 'replica') {
-                history.pushState({ end: true }, '', '#end');
-                await changeMode(old_mode, null);
+                history.pushState({ end: true, replica: false }, '', '#end');
+                await changeMode(old_mode, new_mode);
                 fillTable();
                 return;
             }
             // Push State
-            history.pushState({ new_mode, old_mode }, '', `#${new_mode}`);
+            history.pushState({ new_mode }, '', `#${new_mode}`);
             changeMode(old_mode, new_mode);
         });
     });
