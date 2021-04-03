168edf3af24f60aa45863bf04303148c6a4df000
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-04-03 12:44:30 -0500

    Delete CHOICES, add getitem and setitem

--- vote/models.py
+++ vote/models.py
@@ -1,40 +1,31 @@
-from django.db import models
-from django.contrib.postgres.fields import ArrayField 
+from django.db.models import (
+  Model, CharField, ForeignKey, PositiveSmallIntegerField, CASCADE
+)
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
-class Competitor(models.Model):
+
+
+class Competitor(Model):
   """
   Competitor model object
   """
-  # Create the choices
-  PERSONAJES = 0
-  OBJETOS = 1
-  IMAGENES = 2
-  TERMINACIONES = 3
-  ACTUALIDAD = 4
-  CHOICES = (
-    (PERSONAJES, 'Personajes contrapuestos'),
-    (OBJETOS, 'Objetos'),
-    (IMAGENES, 'Imágenes vaiadas'),
-    (TERMINACIONES, 'Terminaciones'),
-    (ACTUALIDAD, 'Temática de actualidad')
-  )
-
-  name = models.CharField(max_length=20)
-  easy = ArrayField(models.PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='Easy Mode')
-  hard = ArrayField(models.PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='Hard Mode')
-  tematicas = ArrayField(models.PositiveSmallIntegerField(), size=7, null=True, blank=True, verbose_name='Tematicas')
-  random_score = ArrayField(models.PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='Random Mode')
-  min1 = ArrayField(models.PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='minuto 1')
-  min2 = ArrayField(models.PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='minuto 2')
-  deluxe = ArrayField(models.PositiveSmallIntegerField(), size=14, null=True, blank=True, verbose_name='Deluxe')
-  replica = ArrayField(models.PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='Replica')
+  name = CharField(max_length=20)
+  easy = ArrayField(PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='Easy Mode')
+  hard = ArrayField(PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='Hard Mode')
+  tematicas = ArrayField(PositiveSmallIntegerField(), size=7, null=True, blank=True, verbose_name='Tematicas')
+  random_score = ArrayField(PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='Random Mode')
+  min1 = ArrayField(PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='minuto 1')
+  min2 = ArrayField(PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='minuto 2')
+  deluxe = ArrayField(PositiveSmallIntegerField(), size=14, null=True, blank=True, verbose_name='Deluxe')
+  replica = ArrayField(PositiveSmallIntegerField(), size=9, null=True, blank=True, verbose_name='Replica')
+  _list = [ 'easy', 'hard', 'tematicas', 'random_score', 'min1', 'min2', 'deluxe', 'replica' ]
 
   def get_sum(self, mode):
     if mode == 'name':
       raise NameError(f'mode can\'t be equal to {mode}')
 
-    i = 0 
+    i = 0
 
     for j in self.__dict__[mode]:
       if j != 9:
@@ -46,8 +37,8 @@ class Competitor(models.Model):
     """
     Will give you the total of the competitor (excluding replica)
     """
-    return (self.get_sum('easy') + 
-            self.get_sum('hard') + 
+    return (self.get_sum('easy') +
+            self.get_sum('hard') +
             self.get_sum('tematicas') +
             self.get_sum('random_score') +
             self.get_sum('min1') +
@@ -57,14 +48,21 @@ class Competitor(models.Model):
   def __str__(self):
     return self.name
 
+  def __getitem__(self, index: int):
+    return self.__dict__[self._list[index]]
 
-class VotingPoll(models.Model):
+  def __setitem__(self, index: int, value: list):
+    self.__dict__[self._list[index]] = value
+    self.save(update_fields=[self._list[index]])
+
+
+class VotingPoll(Model):
   """
   VotingPoll model object
   """
-  
-  comp_1 = models.ForeignKey(Competitor, on_delete=models.CASCADE, related_name='comp_1')
-  comp_2 = models.ForeignKey(Competitor, on_delete=models.CASCADE, related_name='comp_2')
+
+  comp_1 = ForeignKey(Competitor, on_delete=CASCADE, related_name='comp_1')
+  comp_2 = ForeignKey(Competitor, on_delete=CASCADE, related_name='comp_2')
 
   def __str__(self):
     return f'{self.comp_1} vs {self.comp_2}'
@@ -87,7 +85,7 @@ class VotingPoll(models.Model):
       return comp_1 if max_num == comp_1 else comp_2
 
     # Normal case
-    if (comp_1.get_total() == comp_2.get_total() 
+    if (comp_1.get_total() == comp_2.get_total()
         or abs(comp_1.get_total() - comp_2.get_total()) < 6):
       return 'Réplica'
 
