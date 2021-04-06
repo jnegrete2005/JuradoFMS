c85edae99b52702aa9e4df16fd3a145f7c1ef4ac
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-04-06 17:00:55 -0500

    Add replica case

--- vote/static/vote/TypeScript/replica.ts
+++ vote/static/vote/TypeScript/replica.ts
@@ -1,7 +1,15 @@
+import { changeMode } from './change_mode.js';
+
 document.getElementById('end-btn').addEventListener('click', () => {
   // If it wasn't a tie, just refresh the browser
   if (document.getElementById('end-btn').dataset.isEnd === 'true') {
     location.assign('http://127.0.0.1:8000/vota/');
     return;
   }
+
+  // Else, display replica
+  document.getElementById('poll-container').classList.remove('d-none');
+  document.getElementById('end-container').classList.add('d-none');
+
+  changeMode(document.getElementById('mode').dataset.current_mode, 'replica');
 });
