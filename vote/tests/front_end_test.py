b9ef12edb8a22386451c72ca95468cbdef3a549b
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-04-24 14:25:59 -0500

    Add test_6 which will check if replica #1 works
    This adds test_6, which will test the first replica. It will check the
    replica mode page, and then make the comps 1 and 2 win, and finally,
    causing a tie. It will check the end-rep-table after each winner case.
    
    This causes several changes:
    - Creates a new function, check_rep_winner
    - Changes in check_mode:
      - Rename from check_input to check_mode
      - Add a more descriptive docstring
      - Add optional arg, check_next
      - Addapt these changes in the rest of the file
    - Changes in add_points:
      - Rename arg table to comp_num
      - Added optional args, deluxe and replica
      - In normal case, clear not selected comp's field
      - Addapt these changes in the rest of the file

--- vote/tests/front_end_test.py
+++ vote/tests/front_end_test.py
@@ -74,14 +74,14 @@ class FrontEndTestCase(LiveServerTestCase):
     next_btn = self.selenium.find_element_by_id('next')
 
     for i in range(7):
-      self.check_inputs(next_btn, i)
+      self.check_mode(next_btn, i)
     
   def test_2(self):
     ''' Check if going back through modes works '''
     prev_btn = self.selenium.find_element_by_id('previous')    
     
     for i in reversed(range(8)):
-      self.check_inputs(prev_btn, i, True)
+      self.check_mode(prev_btn, i, True)
 
   def test_3(self):
     ''' Check if the end table works '''
@@ -118,7 +118,7 @@ class FrontEndTestCase(LiveServerTestCase):
   def test_4(self):
     ''' Will check if the table shows comp_1 as winner '''
     # Give 8 points to comp_1
-    self.add_points('comp-1-input', 2)
+    self.add_points(1, 2)
 
     # Check if the table has the correct info
     self.check_table('comp-1-table', 8, 'si')
@@ -126,18 +126,81 @@ class FrontEndTestCase(LiveServerTestCase):
   def test_5(self):
     ''' Will check if the table shows comp_2 as winner '''
     # Give 16 points to comp_2
-    self.add_points('comp-2-input', 4)
+    self.add_points(2, 2)
 
     # Check if the table has the correct info
-    self.check_table('comp-2-table', 16, 'no')
+    self.check_table('comp-2-table', 8, 'no')
 
-  def check_inputs(self, btn, i, reverse = False):
-    ''' Checks if the mode pages are shown properly '''
+  def test_6(self):
+    ''' Checks if replica #1 works in all aspects '''
+    # Give comp_1 enough points to be a replica
+    self.add_points(replica=True)
+
+    self.check_table('comp-1-table', 0, 'Réplica')
+
+    # Click replica btn
+    self.selenium.find_element_by_id('end-btn').click()
+
+    # Check the page without going to the next mode
+    self.check_mode(None, 9, False, False)
+
+    # Make comp_1 win
+    self.check_rep_winner('si', 8, 0)
+
+    # Make comp_2 win
+    self.check_rep_winner('no', 0, 8)
+
+    # Make first replica
+    self.check_rep_winner(winner='Réplica', replica=True)
+
+
+  def check_rep_winner(self, winner: str, comp_1_total = 0, comp_2_total = 0, replica = False):
+    '''
+    Will make a winner and check it in the rep table
+    `comp_num_total` will be divided by 4 to `add_points`
+    to each of the competitors. Then, it will go to the
+    table. Finally, it will check each of the cells using the
+    data given.
+    '''
+    # Add the points to the comps
+    if replica:
+      self.add_points(deluxe=False ,replica=True)
+    else:
+      self.add_points(1, int(comp_1_total / 4), False)
+      self.add_points(2, int(comp_2_total / 4), False)
+
+    # Check the values of the table
+    comps = ['si', 'no']
+    for i in range(2):
+      inputs = list(map(lambda comp_el : comp_el.text, self.selenium.find_elements_by_class_name(f'comp-{i + 1}-rep')))
+
+      if i == 0:
+        self.assertEqual(inputs[0], comps[i])
+        self.assertEqual(inputs[1], str(comp_1_total))      
+        continue
+
+      self.assertEqual(inputs[0], comps[i])
+      self.assertEqual(inputs[1], str(comp_2_total))
+
+    # Check the winner in the table
+    self.assertEqual(self.selenium.find_element_by_id('rep-winner').text, winner)
+
+
+  def check_mode(self, btn, i, reverse = False, check_next = True):
+    '''
+    Checks if the mode page is shown properly
+    by checking the navbar, the mode el and its
+    innerText, length of the inputs, and then,
+    goes to the next mode by clicking the `btn`
+    that was passed.
+    '''
     # Check the navbar
     self.assertFalse('disabled' in self.navs[i].get_attribute('class'))
     self.assertTrue('active' in self.navs[i].get_attribute('class'))
 
     mode = self.selenium.find_element_by_id('mode')
+
+    sleep(0.5)
     
     # Check if the mode is ok
     self.assertEqual(mode.get_attribute('data-current_mode'), get_key_by_val(modes_to_int, i))
@@ -154,8 +217,11 @@ class FrontEndTestCase(LiveServerTestCase):
     else:
       self.assertEqual(len(inputs), 9 * 2)
 
-    if i == 7:
+    if i == 7 or i == 9:
       self.assertEqual(self.selenium.find_element_by_id('next').get_attribute('value'), 'Terminar')
+
+    if not check_next:
+      return sleep(0.5)
 
     if reverse:
       if i != 0:
@@ -166,15 +232,30 @@ class FrontEndTestCase(LiveServerTestCase):
     btn.click()
     sleep(0.5)
 
-  def add_points(self, table: str, num: int):
+  def add_points(self, comp_num: int = None, num: int = 4, deluxe = True, replica = False):
     ''' Will add `num` * 4 points to the competitor you specify by `table` '''
-    # Go to deluxe
-    self.navs[7].click()
+    if deluxe:
+      self.navs[7].click()
+    else:
+      self.navs[9].click()
+
+    sleep(1)
+
+    if replica:
+      inputs = [self.selenium.find_elements_by_class_name('comp-1-input'), self.selenium.find_elements_by_class_name(f'comp-2-input')]
+      for i in range(4):
+        inputs[0][i].clear()
+        inputs[1][i].clear()
+      self.selenium.find_element_by_id('next').click()
+      return sleep(1)
+
+    other_comp = { 1: 2, 2: 1 }
 
     # Give the points to the competitor and got the table
-    inputs = self.selenium.find_elements_by_class_name(table)
+    inputs = [self.selenium.find_elements_by_class_name(f'comp-{comp_num}-input'), self.selenium.find_elements_by_class_name(f'comp-{other_comp[comp_num]}-input')]
     for i in range(num):
-      inputs[i].send_keys(4)
+      inputs[0][i].send_keys(4)
+      inputs[1][i].clear()
     self.selenium.find_element_by_id('next').click()
 
     sleep(1)
