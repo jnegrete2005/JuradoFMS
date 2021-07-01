c0a1ef562f0e108b8b68afbbd3b1ea3a91f0b5dd
Joaquin Negrete <joaquin.negrete03@gmail.com>
2021-07-01 15:20:43 -0500

    Minify documents for faster reloading

--- vote/views.py
+++ vote/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return render(request, 'vote/index.html')
+  return render(request, 'vote/index.min.html')
   
